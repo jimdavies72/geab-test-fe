89efe39d0bd85e2cabbb4d295aa8a8f63309c4c7
James D <jimdavies72@googlemail.com>
2022-05-18 14:39:22 +0100

    initial dev boiler plate added

---
+++ src/components/dataItem/dataItem.js
@@ -0,0 +1,9 @@
+import "./dataItem.css";
+
+export const DataItem = () => {
+  return (
+    <>
+      <span>Data Item</span>
+    </>
+  );
+};
---
+++ src/components/dataList/dataList.js
@@ -0,0 +1,14 @@
+import "./dataList.css";
+
+import { DataItem } from "../dataItem/dataItem";
+
+export const DataList = () => {
+  return (
+    <>
+      <span>Data List</span>
+      <div>
+        <DataItem />
+      </div>
+    </>
+  );
+};
