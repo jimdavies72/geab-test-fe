3af0b7cf54e82a30515bdd3595c42a2fc5fa71a0
James D <jimdavies72@googlemail.com>
2022-05-19 14:00:05 +0100

    added grouped by type

--- src/components/dataList/dataList.js
+++ src/components/dataList/dataList.js
@@ -5,15 +5,18 @@ import { DataItem } from "../dataItem/dataItem";
 
 export const DataList = () => {
   const [products, setProducts] = useState([]);
-  const [buttonValue, setButtonValue] = useState([1]);
+  const [groups, setGroups] = useState([]);
+  const [listType, setListType] = useState("price");
+  const [listOrder, setListOrder] = useState(1);
+  const [listTitle, setListTitle] = useState("Price List - Ascending");
 
   const getProducts = async () => {
     let endpoint;
 
-    if (buttonValue < 2) {
+    if (listType === "price") {
       // price list
       endpoint = "products/price";
-    } else if (buttonValue === 2) {
+    } else if (listType === "group") {
       // group
       endpoint = "products/group";
     } else {
@@ -21,45 +24,81 @@ export const DataList = () => {
       return;
     }
 
-    const payload = { sortOrder: buttonValue };
+    const payload = { sortOrder: listOrder };
     const httpVerb = "PUT";
     const data = await fetchRequest(endpoint, payload, httpVerb);
 
-    setProducts(data.products);
+    if (listType === "group") {
+      setGroups(data.groups);
+      //console.log(groups);
+    } else {
+      setProducts(data.products);
+    }
+  };
+
+  const handleTitle = () => {
+    let sortOrder = "Ascending";
+    if (listOrder === -1) {
+      sortOrder = "Descending";
+    }
+
+    let type = "Price List";
+    if (listType === "group") {
+      type = "Grouped by Type";
+    }
+
+    setListTitle(`${type} - ${sortOrder}`);
   };
 
   useEffect(() => {
     getProducts();
-  }, [buttonValue]);
+    handleTitle();
+  }, [listOrder, listType]);
 
-  const handleButtonClick = (val) => {
-    setButtonValue(val);
+  const handleAscDecClick = (val) => {
+    setListOrder(val);
+  };
+
+  const handleTypeClick = (val) => {
+    setListType(val);
   };
 
   return (
     <div className="list-container">
-      <h1>Products</h1>
+      <h1>{listTitle}</h1>
       <header>
         <h3>Id</h3>
         <h3>Name</h3>
         <div className="price-group">
           <h3>Price</h3>
           <div className="price-btns">
-            <button onClick={() => handleButtonClick(-1)}>Dec</button>
-            <button onClick={() => handleButtonClick(1)}>Asc</button>
+            <button onClick={() => handleAscDecClick(-1)}>Dec</button>
+            <button onClick={() => handleAscDecClick(1)}>Asc</button>
           </div>
         </div>
         <div className="type-group">
           <h3>Type</h3>
-          <button onClick={() => handleButtonClick(2)}>Group</button>
+          <button onClick={() => handleTypeClick("price")}>Price</button>
+          <button onClick={() => handleTypeClick("group")}>Group</button>
         </div>
         <h3>Dept</h3>
         <h3>Weight</h3>
       </header>
       <section>
-        {products.map((product, index) => (
-          <DataItem key={index} product={product} />
-        ))}
+        {listType === "price"
+          ? products.map((product, index) => (
+              <DataItem key={index} product={product} />
+            ))
+          : groups.map((group, index) => (
+              <div key={index}>
+                <h3>{group._id}</h3>
+                <ul>
+                  {group.products.map((product, ind) => (
+                    <DataItem key={ind} product={product} />
+                  ))}
+                </ul>
+              </div>
+            ))}
       </section>
     </div>
   );
